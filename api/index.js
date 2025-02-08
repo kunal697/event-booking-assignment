bf7733e5d5a5adb4ab61d4d27e88b9409b61a69b
Kunal Bodke <132817279+kunal697@users.noreply.github.com>
2025-02-08 19:54:52 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_here";
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use();
 
 // MongoDB Connection with Error Handling
 mongoose.connect(process.env.MONGO_URL)
