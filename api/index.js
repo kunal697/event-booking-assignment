1c7553c37e9284fd78e43dbc36185baabdb79ab6
Kunal Bodke <132817279+kunal697@users.noreply.github.com>
2025-02-09 02:09:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -45,9 +45,6 @@ app.use(express.json());
 mongoose.connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Connected to MongoDB");
-    server.listen(4000, () => {
-      console.log('Server is running on port 4000');
-    });
   })
   .catch((err) => {
     console.error('MongoDB connection error:', err);
